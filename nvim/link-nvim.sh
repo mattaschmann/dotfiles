56723331037e0f905f73dca816fa37f5bba32bec
Matt Aschmann <matt.aschmann@gmail.com>
2018-03-13 20:44:06 -0600

    Add dotbot for better dotfiles installation

--- nvim/link-nvim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-ln -s $DIR/init.vim ~/.config/nvim/init.vim
-ln -s $DIR/ginit.vim ~/.config/nvim/ginit.vim
--- tmux/link-tmux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-ln -s $DIR/.tmux.conf ~/.tmux.conf
