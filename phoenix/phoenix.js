430754ed2bbf52a1fa6ff29c1f5d7fdf8a53a799
Matt Aschmann <maschmann@contractor.usgs.gov>
2018-06-14 10:15:10 -0600

    Add centering back to phoenix

--- phoenix/phoenix.js
+++ phoenix/phoenix.js
@@ -114,9 +114,11 @@ w.prototype.to = function (dir) {
   if ([E, NE, SE].includes(dir)) frame.x += screen.width() - frame.width
   if ([SE, SW].includes(dir))    frame.y += screen.height() - frame.height
   if (F === dir)                 frame.width = screen.width()
-  if ([F, E, W, C].includes(dir))   frame.height = screen.height()
+  if ([F, E, W].includes(dir))   frame.height = screen.height()
   if (C === dir) {
+    frame.height = screen.height() - 2 * (screen.height() / 15)
     frame.x += (screen.width() / 4)
+    frame.y += (screen.height() / 15)
   }
 
   this.setFrame(frame)
