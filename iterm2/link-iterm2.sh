861822a3bfab77d1feb8d9093dc5992bfa1ef8b1
Matt Aschmann <matt.aschmann@gmail.com>
2020-03-26 16:51:15 -0600

    Add missing antibody plugin to avoid error

--- iterm2/link-iterm2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DIR"
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
--- zsh/.zsh_plugins.sh
+++ zsh/.zsh_plugins.sh
@@ -7,6 +7,8 @@ source /home/ma7hatter/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLAS
 fpath+=( /home/ma7hatter/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/docker-compose )
 
 
+source /home/ma7hatter/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /home/ma7hatter/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
 source /home/ma7hatter/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
 fpath+=( /home/ma7hatter/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
 source /home/ma7hatter/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
