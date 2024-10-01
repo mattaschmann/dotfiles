fa1bdda4df131eb2c3eb9bdb42d5c888586e1f19
Matt Aschmann <matt.aschmann@gmail.com>
2024-10-01 14:19:01 -0600

    feat: add nvim-scrollview

---
+++ nvim/lua/plugins/nvim-scrollview.lua
@@ -0,0 +1,20 @@
+return {
+  'https://github.com/dstein64/nvim-scrollview',
+  opts = {
+    signs_on_startup = {
+      -- 'changelist',   -- change list items (previous, current, and next)
+      'conflicts',    -- git merge conflicts
+      -- 'cursor',       -- cursor position
+      'diagnostics',  -- errors, warnings, info, and hints
+      'folds',        -- closed folds
+      -- 'latestchange', -- latest change
+      'loclist',      -- items on the location list
+      'marks',
+      'quickfix',     -- items on the quickfix list
+      'search',
+      'spell',        -- spell check items when the spell option is enabled
+      'textwidth',    -- line lengths exceeding the value of the textwidth option, when non-zero
+      'trail',        -- trailing whitespace
+    },
+  },
+}
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -7,6 +7,7 @@ return {
     configs.setup({
       ensure_installed = {
         'bash',
+        'java',
         'javascript',
         'json',
         'lua',
