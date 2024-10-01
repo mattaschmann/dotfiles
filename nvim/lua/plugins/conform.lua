94255d476c01ddec15766f1ce00bcd08384d4d5f
Matt Aschmann <matt.aschmann@gmail.com>
2024-10-01 10:57:31 -0600

    fix: fallback to lsp formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,6 +4,9 @@ return {
     local conform = require('conform')
 
     conform.setup({
+      default_format_opts = {
+        lsp_format = 'fallback',
+      },
       formatters_by_ft = {
         lua = { 'stylua' },
         python = { 'isort', 'black' },
