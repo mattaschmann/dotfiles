52f0090df9a22acf119eece8bdef5465b3db9a4f
Matt Aschmann <matt.aschmann@gmail.com>
2024-10-21 10:44:26 -0600

    fix: autoclose

--- nvim/lua/plugins/autoclose.lua
+++ nvim/lua/plugins/autoclose.lua
@@ -1,9 +1,6 @@
 return {
   'https://github.com/m4xshen/autoclose.nvim',
   opts = {
-    keys = {
-      ["'"] = { disabled_filetypes = { 'markdown' } },
-      ['"'] = { disabled_filetypes = { 'markdown' } },
-    },
+    disabled_filetypes = { 'text', 'markdown' },
   },
 }
