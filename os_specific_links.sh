aa162b8077f1eff2bb2933bba9e6a4c6bebf6b6d
Matt Aschmann <matt.aschmann@gmail.com>
2026-03-09 11:28:07 -0600

    fix os specific links

---
+++ os_specific_links.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ "$(uname)" == "Darwin" ]]; then #osx
+  echo "OSX detected"
+  export OS_NAME="osx"
+elif [[ "$(uname -r)" == *"WSL2" ]]; then #wsl
+  echo "WSL detected"
+  export OS_NAME="wsl"
+fi
+
+ln -fs $(pwd)/alacritty/alacritty.${OS_NAME}.toml ~/.config/alacritty/alacritty.toml
