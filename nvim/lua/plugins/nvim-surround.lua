0223a732f8e211e42b203a1146e3520e8ee90ec6
Matt Aschmann <matt.aschmann@gmail.com>
2026-03-10 09:00:26 -0600

    fix: migrate nvim-surround

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -2,9 +2,7 @@ return {
   'https://github.com/kylechui/nvim-surround',
   version = '*', -- Use for stability; omit to use `main` branch for the latest features
   event = 'VeryLazy',
-  opts = {
-    keymaps = {
-      visual = 'gs',
-    },
-  },
+  config = function()
+    vim.keymap.set({ 'v' }, 'gs', '<Plug>(nvim-surround-visual)')
+  end
 }
