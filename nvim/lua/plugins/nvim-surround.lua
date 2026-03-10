131af8092f674619aaa1d91486a8f1936ba849b8
Matt Aschmann <matt.aschmann@gmail.com>
2026-03-10 09:01:25 -0600

    remove unused line

--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -1,6 +1,5 @@
 return {
   'https://github.com/kylechui/nvim-surround',
-  version = '*', -- Use for stability; omit to use `main` branch for the latest features
   event = 'VeryLazy',
   config = function()
     vim.keymap.set({ 'v' }, 'gs', '<Plug>(nvim-surround-visual)')
